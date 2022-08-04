40c1b05c1bef84cf6ff42db82ad7a1d3dc7aafdc
hardippatel7 <hardippatel7@gmail.com>
2022-08-04 12:17:34 -0400

    commit 1 logout button

--- hardipRoshni_COMP304Sec001_Lab5_Group11/app/src/main/java/com/example/hardiproshni_comp304sec001_lab5_group11/SecondActivity.java
+++ hardipRoshni_COMP304Sec001_Lab5_Group11/app/src/main/java/com/example/hardiproshni_comp304sec001_lab5_group11/SecondActivity.java
@@ -2,13 +2,25 @@ package com.example.hardiproshni_comp304sec001_lab5_group11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private Button logoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent i = new Intent(SecondActivity.this,MainActivity.class);
+            startActivity(i);
+        });
     }
 }
